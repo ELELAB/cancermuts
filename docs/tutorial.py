c72b619e9f8f58f92cbb1778c013bc1d1eb4b212
Matteo Tiberti <matteo.tiberti@gmail.com>
2022-05-26 22:05:47 +0200

    added prints

--- docs/tutorial.py
+++ docs/tutorial.py
@@ -27,6 +27,18 @@ cb = cBioPortal(cancer_studies=['coadread_dfci_2016',
 
 cb.add_mutations(seq, metadata=['cancer_type', 'cancer_study', 'genomic_mutations'])
 
+# let us check out some of the mutations
+print(seq.positions[38].mutations)
+print(seq.positions[64].mutations)
+print(seq.positions[122].mutations)
+
+print(seq.positions[64].mutations[0].sources)
+
+print(seq.positions[64].mutations[0].mutated_residue_type)
+
+print(seq.positions[38].mutations[0].metadata)
+
+
 # add mutations from COSMIC
 cosmic = COSMIC(database_files=['/data/databases/cosmic-v95/CosmicMutantExport.tsv'],
                 database_encoding=['latin1'])
@@ -39,11 +51,21 @@ cosmic.add_mutations(seq,
 					 metadata=['genomic_coordinates', 'genomic_mutations', 
 					 			'cancer_site', 'cancer_histology'])
 
+
+# let's check them out
+print(seq.positions[64].mutations[0])
+
+print(seq.positions[64].mutations[0].sources)
+
+print(seq.positions[64].mutations[0].metadata)
+                                         
 # add annotations from MyVariant (REVEL)
 from cancermuts.datasources import MyVariant
 
 mv = MyVariant()
 mv.add_metadata(seq)
+
+print(seq.positions[64].mutations[0].metadata['revel_score'])
 
 # add annotations from gnomAD
 from cancermuts.datasources import gnomAD
@@ -52,15 +74,27 @@ gnomad = gnomAD(version='2.1')
 gnomad.add_metadata(seq, md_type=['gnomad_exome_allele_frequency',
 	                              'gnomad_genome_allele_frequency'])
 
+print(seq.positions[64].mutations[0].metadata['gnomad_exome_allele_frequency'])
+
+print(seq.positions[64].mutations[0].metadata['gnomad_genome_allele_frequency'])
+
 from cancermuts.datasources import PhosphoSite, MobiDB
 
 # add annotations from PhosphoSite
 ps = PhosphoSite('/data/databases/phosphosite/')
 ps.add_position_properties(seq)
 
+print(seq.positions[4].properties)
+
+print(seq.positions[28].properties)
+
 # add annotations from MobiDB
 mdb = MobiDB()
 mdb.add_position_properties(seq)
+
+print(seq.positions[0].properties['mobidb_disorder_propensity'])
+
+print(seq.positions[10].properties['mobidb_disorder_propensity'])
 
 # add annotations from ELM
 from cancermuts.datasources import ELMPredictions
@@ -68,6 +102,9 @@ from cancermuts.datasources import ELMPredictions
 elm = ELMPredictions()
 elm.add_sequence_properties(seq,
 			    exclude_elm_classes="MOD_.")
+
+print(seq.properties[0:5])
+print(seq.properties['linear_motif'][0].type)
 
 # save table
 from cancermuts.table import Table
