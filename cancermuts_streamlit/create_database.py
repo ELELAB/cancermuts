4a89a77c09228a9420b308a8615a802522ef4540
Alberte Estad <ahestad@outlook.com>
2023-11-04 14:04:48 +0100

    create_database.py now checks if there are several files starting with metatable and ending with .csv and stops if true

--- cancermuts_streamlit/create_database.py
+++ cancermuts_streamlit/create_database.py
@@ -20,6 +20,7 @@ import pandas as pd
 from pathlib import Path
 import streamlit_utils as su
 from bioservices.uniprot import UniProt
+import collections
 
 datadir = "/data/raw_data/computational_data/cancermuts_data/"
 entry_details_file = "example_entries.csv"
@@ -27,7 +28,7 @@ database_dir = "example_database"
 
 entry_details_df = pd.read_csv(entry_details_file)
 
-metatable = pd.DataFrame(columns=['Gene', 'Uniprot ID', 'Date', 'Source'])
+rows = collections.defaultdict(list)
 
 for index, row in entry_details_df.iterrows():
     path = os.path.join(datadir, row['protein_class'], row['protein_name'], row['run_type'])
@@ -42,27 +43,31 @@ for index, row in entry_details_df.iterrows():
                     most_recent = date_obj
                 
         dir_path = os.path.join(path, most_recent.strftime("%d%m%Y"))
-        filename = [filename for filename in os.listdir(dir_path) if filename.startswith('metatable')][0]
-        file_path = os.path.join(dir_path, filename)
+        filename = [filename for filename in os.listdir(dir_path) if filename.startswith('metatable') and filename.endswith('.csv')]
+        
+        if len(filename) > 1:
+            print("More than one Cancermuts metatable was found in the path: " + dir_path)
+            break
 
-        if os.path.isfile(file_path) and not os.path.isfile(os.path.join(database_dir, filename)):
+        file_path = os.path.join(dir_path, filename[0])
+
+        if os.path.isfile(file_path) and not os.path.isfile(os.path.join(database_dir, filename[0])):
             file = pd.read_csv(file_path)
             file.to_csv(os.path.join(database_dir, row['protein_name']))
-            #file.to_csv(os.path.join(database_dir, row['protein_name'] + '_' + most_recent.strftime("%d%m%Y")))
 
-            metatable_row = []
-            metatable_row.append(row['protein_name'])
             u = UniProt(verbose=False)
             up_acc = u.search(row['protein_name']+"+and+taxonomy_id:9606", limit=3, columns="accession")
-            metatable_row.append(up_acc.split('\n')[1])
-            metatable_row.append(most_recent)
-            metatable_row.append(row['Reference'])
-            metatable.loc[len(metatable)] = metatable_row
+
+            rows['Gene'].append(row['protein_name'])
+            rows['Uniprot ID'].append(up_acc.split('\n')[1])
+            rows['Date'].append(most_recent)
+            rows['Source'].append(row['Reference'])
 
     else:
         print(f"Entry not found: {path} is not a directory")
 
-metatable.to_csv(os.path.join(database_dir, 'index_table.csv'), index=False)
+df = pd.DataFrame.from_dict(rows)
+df.to_csv(os.path.join(database_dir, 'index_table.csv'), index=False)
 
 
 
