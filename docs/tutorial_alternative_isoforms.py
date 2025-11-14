aeb1299a6ed1a3908cefbdba1b335a6032346a21
Matteo Tiberti <matteo.tiberti@gmail.com>
2025-11-14 15:29:44 +0100

    moved revel section forward, after annotating all mutations

--- docs/tutorial_alternative_isoforms.py
+++ docs/tutorial_alternative_isoforms.py
@@ -52,20 +52,6 @@ else:
     print("No COSMIC mutations found for transcript:",
           seq.aliases.get('transcript_accession') or seq.aliases.get('ensembl_transcript_id') or "canonical")
 
-
-#print(seq.positions[819].mutations[0])
-#print(seq.positions[819].mutations[0].sources)
-#print(seq.positions[819].mutations[0].metadata)
-
-# annotate with REVEL using local database
-rl = RevelDatabase("/data/databases/REVEL/revel_with_transcript_ids")
-rl.add_metadata(seq)
-
-# print annotated mutation and REVEL score
-mut = seq.positions[819].mutations[0]
-print("Mutation:", mut)
-print("REVEL score:", mut.metadata.get('revel_score', []))
-
 # cBioPortal does not support non-canonical mutations
 cbioportal = cBioPortal()
 
@@ -107,6 +93,19 @@ if positions_with_cv:
         print("Last ClinVar mutation:", last_mut, "at position", last_pos.sequence_position)
 else:
     print("No ClinVar mutations found for refseq:", seq.aliases['refseq'])
+
+#print(seq.positions[819].mutations[0])
+#print(seq.positions[819].mutations[0].sources)
+#print(seq.positions[819].mutations[0].metadata)
+
+# annotate with REVEL using local database
+rl = RevelDatabase("/data/databases/REVEL/revel_with_transcript_ids")
+rl.add_metadata(seq)
+
+# print annotated mutation and REVEL score
+mut = seq.positions[819].mutations[0]
+print("Mutation:", mut)
+print("REVEL score:", mut.metadata.get('revel_score', []))
 
 # PhosphoSite does not support non-canonical isoforms
 ps = PhosphoSite('/data/databases/phosphosite/')
